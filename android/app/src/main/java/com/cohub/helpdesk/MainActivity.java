0c185f77409c99db96d4382ab4216eb4199a7664
Addiyy <mad.itya1802@gmail.com>
2025-08-20 19:53:51 +0530

    capacitor config

---
+++ android/app/src/main/java/com/cohub/helpdesk/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cohub.helpdesk;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cohub.helpdesk',
+  appName: 'Cohub Manager',
+  webDir: 'dist'
+};
+
+export default config;
